4ced27253c81e80b39bbdcbd579c493cee90c888
José Valim <jose.valim@plataformatec.com.br>
2014-10-28 12:47:48 -0200

    Move to one Task per event to be processed

--- lib/work_queue.ex
+++ lib/work_queue.ex
@@ -3,9 +3,9 @@ defmodule WorkQueue do
   use PipeWhileOk
 
   require Logger
-  
+
   alias WorkQueue.Options
-  
+
   @doc File.read!("README.md")
 
   def process(worker_fn, item_source, extra_opts \\ []) do
@@ -13,7 +13,7 @@ defmodule WorkQueue do
       package_parameters(worker_fn, item_source, extra_opts)
       |> Options.analyze
       |> start_workers
-      |> schedule_work 
+      |> schedule_work
     end
   end
 
@@ -23,8 +23,7 @@ defmodule WorkQueue do
           worker_fn:        worker_fn,
           item_source:      item_source,
           opts:             extra_opts,
-          results:          [],
-          running_workers:  0,
+          results:          []
        }
     }
   end
@@ -35,61 +34,62 @@ defmodule WorkQueue do
 
   defp schedule_work(params) do
     params.opts.report_progress_to.({:started, nil})
-    
-    params = Dict.put(params, :running_workers, params.opts.worker_count)
 
     results = if params.opts.report_progress_interval do
-                loop_with_ticker(params)
+                loop_with_ticker(params, [], params.opts.worker_count)
               else
-                loop(params)
+                loop(params, [], params.opts.worker_count)
               end
 
     params.opts.report_progress_to.({:finished, results})
     results
   end
 
-  defp loop_with_ticker(params) do
+  defp loop_with_ticker(params, running, max) do
     {:ok, ticker} = :timer.send_interval(params.opts.report_progress_interval,
                                          self, :tick)
-    count = loop(params)
+    count = loop(params, running, max)
     :timer.cancel(ticker)
     count
   end
 
-  defp loop(params) do
-    receive do
-      { :send_work, worker } ->
-        {status, params, next_item} = get_next_item(params)
-        WorkQueue.Worker.process(worker, status, next_item)
-        loop(params)
+  defp loop(params, running, max) when length(running) < max do
+    case get_next_item(params) do
+      {:done, params, _} when running == [] ->
+        Process.unlink(params.supervisor_pid)
+        Process.exit(params.supervisor_pid, :shutdown)
+        params.results
 
-      { :processed, worker, { :ok, result } } ->
-        params = update_in(params[:results], &[result|&1])
-        params.opts.report_each_result_to.(result)
-        {status, params, next_item} = get_next_item(params)
-        WorkQueue.Worker.process(worker, status, next_item)
-        loop(params)
+      {:done, params, _} ->
+        wait_for_answers(params, running, max)
 
-      { :shutdown, _worker } ->
-        if params.running_workers > 1 do
-          loop(%{params | running_workers: params.running_workers - 1})
-        else
-          Process.flag(:trap_exit, true)
-          Process.exit(params.supervisor_pid, :shutdown)
-          params.results
-        end
-
-      :tick ->
-        params.opts.report_progress_to.({:progress, length(params.results)})
-        loop(params)
-
+      {:ok, params, item} ->
+        {:ok, worker} = WorkQueue.Worker.process(params, self(), item)
+        loop(params, [worker|running], max)
     end
   end
 
+  defp loop(params, running, max) do
+    wait_for_answers(params, running, max)
+  end
+
+  defp wait_for_answers(params, running, max) do
+    receive do
+      :tick ->
+        params.opts.report_progress_to.({:progress, length(params.results)})
+        loop(params, running, max)
+      { :processed, worker, { :ok, result } } ->
+        if worker in running do
+          params = update_in(params.results, &[result|&1])
+          loop(params, List.delete(running, worker), max)
+        else
+          loop(params, running, max)
+        end
+    end
+  end
 
   defp get_next_item(params) do
     {status, item, new_state}  = params.opts.get_next_item.(params.item_source)
     {status, Dict.put(params, :item_source, new_state), item}
   end
-  
 end
